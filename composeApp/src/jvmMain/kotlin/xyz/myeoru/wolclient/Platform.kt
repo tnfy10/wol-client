239329b6e8344a05194f6909737979f2b42a3973
Hong Sunghwi <tnfy10@gmail.com>
2025-12-01 17:09:38 +0900

    Initial commit

---
+++ composeApp/src/jvmMain/kotlin/xyz/myeoru/wolclient/Platform.kt
@@ -0,0 +1,7 @@
+package xyz.myeoru.wolclient
+
+class JVMPlatform {
+    val name: String = "Java ${System.getProperty("java.version")}"
+}
+
+fun getPlatform() = JVMPlatform()
---
+++ composeApp/src/jvmMain/kotlin/xyz/myeoru/wolclient/main.kt
@@ -0,0 +1,13 @@
+package xyz.myeoru.wolclient
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "WOL-Client",
+    ) {
+        App()
+    }
+}
